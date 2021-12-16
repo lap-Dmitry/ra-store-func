b553bd4bafd0c63569f6bb01159748b407bc01a0
Dmitry Lapshin <76706628+lap-Dmitry@users.noreply.github.com>
2021-12-16 10:41:51 +0300

    Add files via upload

---
+++ src/components/ShopItemFunc.js
@@ -0,0 +1,28 @@
+import React from "react";
+import propTypes from "prop-types";
+import ItemModel from "../models/ItemModel";
+
+export default function ShopItemFunc(props) {
+    const { item } = props;
+
+    return (
+        <div class="main-content">
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>
+            <div class="description">{item.descriptionFull}</div>
+            <div class="highlight-window mobile">
+            <div class="highlight-overlay"></div>
+            </div>
+            <div class="divider"></div>
+            <div class="purchase-info">
+                <div class="price">{item.currency}{item.price}</div>
+                <button>Добавить в корзину</button>
+            </div>
+        </div>
+    );
+}
+
+ShopItemFunc.propTypes = {
+    item: propTypes.instanceOf(ItemModel).isRequired
+}
